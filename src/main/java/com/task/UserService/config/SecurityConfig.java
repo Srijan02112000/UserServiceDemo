548fb5515ad7d51f06fa702b6fdb880cb843d47d
Srijan Ram S N <205140@ibsplc.com>
2025-03-29 20:50:52 +0530

    URL update in security filterchain

--- src/main/java/com/task/UserService/config/SecurityConfig.java
+++ src/main/java/com/task/UserService/config/SecurityConfig.java
@@ -53,8 +53,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/delete").hasRole("ADMIN")
-                        .requestMatchers("/api/users/all").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/users/delete").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/users/all").hasRole("ADMIN")
                         .requestMatchers(
                                 new AntPathRequestMatcher("/swagger-ui/**"),
                                 new AntPathRequestMatcher("/v3/api-docs/**"),
